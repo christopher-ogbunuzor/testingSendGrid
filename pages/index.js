4144a298fd6ba743aa25ad919c468f0f960f6fbc
christopher-ogbunuzor <93588048+christopher-ogbunuzor@users.noreply.github.com>
2024-01-26 20:33:59 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  async function handleOnSubmit(e) {
+  async function handleOnSubmit(e) { 
     e.preventDefault();
 
     const formData = {};
